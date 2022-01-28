c11aca532751e7fdd9d442fec08f1a06c0daac4b
captaincsk <63608833+captaincsk@users.noreply.github.com>
2022-01-28 13:16:55 +0530

    Add files via upload

---
+++ positiveornegative.js
@@ -0,0 +1,15 @@
+const num = 10 ;
+
+if (num > 0) {
+    console.log(" number is positive");
+}
+
+else if (num == 0) {
+  console.log(" number is zero");
+}
+
+else {
+     console.log(" number is negative");
+}
+
+// i gave single input , you can change and check
